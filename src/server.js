2ebac05e6f0041f09aac06c293b32df7e9cfc959
Alenwuhl <128154613+Alenwuhl@users.noreply.github.com>
2023-12-03 13:35:14 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { __dirname } from "./dirname.js";
+import handlebars from "express-handlebars";
+import viewsRouter from "./routes/views.routes.js";
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = 5000;
+
+const httpServer = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+const io = new Server(httpServer);
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + "/public"));
+
+app.use("/", viewsRouter);
+
+const messages = [];
+
+io.on("connection", (socket) => {
+  console.log("Nuevo usuario conectado");
+  socket.broadcast.emit("newConnection", (data) => {
+    console.log(data);
+  });
+
+  socket.on("message", (data) => {
+    console.log(data);
+    messages.push(data);
+    io.emit("messages", messages);
+  });
+
+  socket.emit("messages", messages);
+});
